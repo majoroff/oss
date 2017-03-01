5fda0bf71f379bf9f121eb25add89b21a6dab012
Sergey Mayorov <majoroff.cc@gmail.com>
2017-03-01 13:53:44 +0300

    stack implementation with linked list

---
+++ 08_ALGO_PT1/src/Stack.java
@@ -0,0 +1,61 @@
+import java.util.Iterator;
+import edu.princeton.cs.algs4.*;
+/**
+ * Created by majoroff on 01.03.17.
+ */
+public class Stack<Item> implements Iterable<Item> {
+
+    private Node first;
+    private int N;
+
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() { return N == 0; }
+    public int size() {return N; }
+
+    public void push(Item item){
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop(){
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext() { return current.next != null; }
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+
+    public static void main(String[] args){
+        Stack<String> s = new Stack<String>();
+        while(!StdIn.isEmpty()){
+            String item = StdIn.readString();
+            if(!item.equals("-")){
+                s.push(item);
+            }
+            else if(!s.isEmpty()) StdOut.print(s.pop() + " ");
+        }
+        StdOut.println("(В стеке осталось " + s.size() + ")");
+    }
+
+}
